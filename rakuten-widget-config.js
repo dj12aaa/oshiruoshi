e72cc22ed5d38644ba582d2ca5608a21f9f642ac
dj12aaa <dj.amuy12@gmail.com>
2026-08-18 21:43:40 +0900

    Add responsive Rakuten affiliate widget configuration

---
+++ rakuten-widget-config.js
@@ -0,0 +1,16 @@
+(() => {
+  'use strict';
+  const compact=window.matchMedia?.('(max-width:520px)').matches===true;
+  window.rakuten_design='slide';
+  window.rakuten_affiliateId='56a9a4c8.29b89c61.56a9a4c9.ae05c2f1';
+  window.rakuten_items='ctsmatch';
+  window.rakuten_genreId='0';
+  window.rakuten_size=compact?'320x48':'468x160';
+  window.rakuten_target='_blank';
+  window.rakuten_theme='gray';
+  window.rakuten_border='off';
+  window.rakuten_auto_mode='on';
+  window.rakuten_genre_title='off';
+  window.rakuten_recommend='on';
+  window.rakuten_ts='1787056711267';
+})();
